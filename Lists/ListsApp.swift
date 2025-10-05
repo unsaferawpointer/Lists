fc61d00fb630036f9c526305989d6713889168c5
Anton Cherkasov <unsaferawpointer@gmail.com>
2025-10-05 15:07:41 +0300

    Initial Commit

---
+++ Lists/ListsApp.swift
@@ -0,0 +1,32 @@
+//
+//  ListsApp.swift
+//  Lists
+//
+//  Created by Anton Cherkasov on 05.10.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ListsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ListsTests/ListsTests.swift
@@ -0,0 +1,16 @@
+//
+//  ListsTests.swift
+//  ListsTests
+//
+//  Created by Anton Cherkasov on 05.10.2025.
+//
+
+import Testing
+
+struct ListsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
