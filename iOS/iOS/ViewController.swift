47d3b9e49dd9c0f9eedf2211e2973ccebe713d0c
Anton Cherkasov <unsaferawpointer@gmail.com>
2025-10-13 21:01:22 +0300

    [mvp]: Add workspace

---
+++ iOS/iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS
+//
+//  Created by Anton Cherkasov on 13.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ iOS/iOSTests/iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTests.swift
+//  iOSTests
+//
+//  Created by Anton Cherkasov on 13.10.2025.
+//
+
+import Testing
+@testable import iOS
+
+struct iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
